2901b07c2391fa10988957ce3f758d2e39afca8c
Daniel Nascimento <danielhn@gmail.com>
2020-07-20 20:52:33 +0000

    Function after ESLint check.

--- tail.js
+++ tail.js
@@ -1,21 +1,11 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
-  let new_array = array.slice(1); 
-  return new_array;
+  let newArray = array.slice(1);
+  return newArray;
 };
 
 const result1 = [];
 const result2 = ["Yo Yo", "Lighthouse", "Labs"];
 const result3 = ["Yo Yo", "Lighthouse", "Labs", "Toronto", "Ontario", "Canada"];
-
 
 console.log(tail(result1));
 console.log(tail(result2));
