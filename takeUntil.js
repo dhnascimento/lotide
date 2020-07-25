59e19861d79756fec19ee808ed4ee97cda6f135c
Daniel Nascimento <danielhn@pop-os.localdomain>
2020-07-25 15:46:06 -0400

    Added and modified some of the tests to check if the arrays created by takeUntil  were being returned as expected.

--- takeUntil.js
+++ takeUntil.js
@@ -18,18 +18,21 @@ const assertArrayEqual = function(array1, array2) {
   } return "✅✅✅ Assertion Passed: the arrays are identical!";
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
+const data1 = [1, 2, 3, 1, 2, 3];
+const results1 = takeUntil(data1, x => x > 2);
 console.log(results1);
 
-console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
-console.log(assertArrayEqual(results1, [ 1, 2, 5, 7, 3 ]));
+
+console.log(assertArrayEqual(results1, [1, 2]));
+
+
 console.log(assertArrayEqual(results1, [ 1, 2, 5, 7, 2 ]));
+
 
 console.log(assertArrayEqual(results2, [ "I've", "been", "to", "Hollywood" ]));
 console.log(assertArrayEqual(results2, [ "I've", "been", "to", "Holly" ]));
